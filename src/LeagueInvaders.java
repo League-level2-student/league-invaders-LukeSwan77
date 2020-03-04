151cd7093129d1af9dd8f8329f520b1e8de7b9ef
League Student <league@iMac-16.attlocal.net>
2020-03-03 16:55:54 -0800

    Finished "Runner" section

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	static JFrame frame;
+	public static final int gameWidth = 500;
+	public static final int gameHeight = 800;
+	public LeagueInvaders() {
+		frame = new JFrame("League Invaders");
+	}
+	
+public static void main(String[] args) {
+	LeagueInvaders invader = new LeagueInvaders();
+	invader.setup();
+	
+}
+
+void setup() {
+	frame.setSize(gameWidth, gameHeight);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 
 }
