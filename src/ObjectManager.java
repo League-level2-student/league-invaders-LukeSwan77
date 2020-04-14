966395ed22ce287a48ca67437ecc8f96c5df7246
LukeSwan77 <52898607+LukeSwan77@users.noreply.github.com>
2020-04-14 15:34:58 -0700

    yep

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,11 +1,13 @@
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class ObjectManager {
 	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 	ArrayList<Alien> aliens = new ArrayList<Alien>();
+	Rocketship rocket;
 	public ObjectManager(Rocketship rock){
-	
+	this.rocket = rock;
 }
 void addProjectile(Projectile pro){
 	projectiles.add(pro);
@@ -20,6 +22,20 @@ void update(){
 			aliens.get(i).isActive = false;
 		}
 	}
-	
+	for (int i = 0; i < projectiles.size(); i++) {
+		projectiles.get(i).update();
+		if(projectiles.get(i).y < 0){
+			projectiles.get(i).isActive = false;
+		}
+	}
+	}
+void draw(Graphics g){
+	rocket.draw(g);
+	for (int i = 0; i < aliens.size(); i++) {
+		aliens.get(i).draw(g);
+	}
+	for (int i = 0; i < projectiles.size(); i++) {
+		projectiles.get(i).draw(g);
+	}
 }
 }
