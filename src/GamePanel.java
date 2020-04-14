843d01a3a2060f748f7fae3a7d303d8c6ea6a9a0
LukeSwan77 <52898607+LukeSwan77@users.noreply.github.com>
2020-04-14 16:57:45 -0700

    left off on collision managment

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,22 +5,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;	
 	final int MENU = 0;
 	final int GAME = 1;
 	final int END = 2;
 	int currentState = MENU;
 	Timer frameDraw;
+	Timer alienSpawn;
 	Font titleFont;
 	Rocketship rocket = new Rocketship(250,500,50,50);
+	ObjectManager manager = new ObjectManager(rocket);
 	GamePanel(){
 		titleFont = new Font("Ariel", Font.PLAIN, 36);
 		frameDraw = new Timer(1000/60,this);
 	    frameDraw.start();
+	    if (needImage) {
+	        loadImage ("space.png");
+	    }
+
 	}
 	@Override
 	public void paintComponent(Graphics g) {
@@ -36,7 +47,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		
 	}
 	void updateGameState() {
-		
+		manager.update();
 	}
 	void updateEndState() { 
 
@@ -49,9 +60,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		g.drawString("LEAGUE INVADERS", 20, 100);
 	}
 	void drawGameState(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, LeagueInvaders.gameWidth, LeagueInvaders.gameHeight);
-		rocket.draw(g);
+		if (gotImage) {
+			g.drawImage(image, 0, 0, LeagueInvaders.gameWidth, LeagueInvaders.gameHeight, null);
+		} else {
+			g.setColor(Color.BLUE);
+			g.fillRect(0, 0, LeagueInvaders.gameWidth, LeagueInvaders.gameHeight);
+		}
+		manager.draw(g);
 	}
 	void drawEndState(Graphics g) {
 		g.setColor(Color.RED);
@@ -76,11 +91,18 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
+		if (arg0.getKeyCode()==KeyEvent.VK_SPACE){
+			manager.addProjectile(rocket.getProjectile());
+		}
 		if (arg0.getKeyCode()==KeyEvent.VK_ENTER) {
 		    if (currentState == END) {
 		        currentState = MENU;
+		    } else if(currentState == MENU) {
+		        currentState = GAME;
+		        startGame();
 		    } else {
-		        currentState++;
+		    	currentState = END;
+		    	alienSpawn.stop();
 		    }
 		}   
 		if (arg0.getKeyCode()==KeyEvent.VK_UP) {
@@ -115,5 +137,19 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		// TODO Auto-generated method stub
 		
 	}
-	
+	void loadImage(String imageFile) {
+	    if (needImage) {
+	        try {
+	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+		    gotImage = true;
+	        } catch (Exception e) {
+	            
+	        }
+	        needImage = false;
+	    }
+	}
+	void startGame(){
+		alienSpawn = new Timer(1000 , manager);
+	    alienSpawn.start();
+	}
 }
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,8 +1,10 @@
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class ObjectManager {
+public class ObjectManager implements ActionListener{
 	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 	ArrayList<Alien> aliens = new ArrayList<Alien>();
 	Rocketship rocket;
@@ -37,5 +39,22 @@ void draw(Graphics g){
 	for (int i = 0; i < projectiles.size(); i++) {
 		projectiles.get(i).draw(g);
 	}
+}
+void purge(){
+	for (int i = 0; i < aliens.size(); i++) {
+		if(aliens.get(i).isActive == false){
+			aliens.remove(i);
+		}
+	}
+	for (int i = 0; i < projectiles.size(); i++) {
+		if(projectiles.get(i).isActive == false){
+			projectiles.remove(i);
+		}
+	}
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	addAlien();
 }
 }
